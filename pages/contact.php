955eea0ef2578d76fcc9dec78590ac5a7c43e9b7
Shahzaib <shahzaibux@gmail.com>
2020-06-29 04:33:31 +0500

    Updated Project

--- pages/contact.php
+++ pages/contact.php
@@ -52,3 +52,14 @@
         </div>
     </div>
 </div>
+<div class="google-map">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div id="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d23481.794658246406!2d21.09932224141022!3d42.635403966818785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x13549de02b95feb3%3A0x40df47fa2a0e9612!2sPrishtina!5e0!3m2!1sen!2s!4v1504188040281" height="520" style="border:none; background:transparent; overflow:hidden; width:100%;"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
