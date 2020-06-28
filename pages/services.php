7db4cc0ad78b3336e28fcd38a4b8ee9bd8247746
Shahzaib <shahzaibux@gmail.com>
2020-06-29 04:43:34 +0500

    Updated Project

--- pages/services.php
+++ pages/services.php
@@ -10,3 +10,75 @@
         </div>
     </div>
 </div>
+<div class="what-we-offer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="section-heading">
+                    <span>WHAT WE OFFER</span>
+                    <h2>here you can find our services</h2>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-item">
+                    <h4>ready to collaboration</h4>
+                    <div class="dev"></div>
+                    <i class="fimanager flaticon-017-collaboration"></i>
+                    <div class="hover-effect">
+                        <p>Lorem ipsum dolor amet you  probably haven't heard of them aster literally jean shorts woke. Food truck post-ironic lovely layout hashtag seitan.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-item">
+                    <h4>Fixes &amp; improvements</h4>
+                    <div class="dev"></div>
+                    <i class="fimanager flaticon-018-corporate"></i>
+                    <div class="hover-effect">
+                        <p>Lorem ipsum dolor amet you  probably haven't heard of them aster literally jean shorts woke. Food truck post-ironic lovely layout hashtag seitan.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-item">
+                    <h4>healthy pertnership</h4>
+                    <div class="dev"></div>
+                    <i class="fimanager flaticon-044-partnership"></i>
+                    <div class="hover-effect">
+                        <p>Lorem ipsum dolor amet you  probably haven't heard of them aster literally jean shorts woke. Food truck post-ironic lovely layout hashtag seitan.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-item">
+                    <h4>safe database</h4>
+                    <div class="dev"></div>
+                    <i class="fimanager flaticon-020-database"></i>
+                    <div class="hover-effect">
+                        <p>Lorem ipsum dolor amet you  probably haven't heard of them aster literally jean shorts woke. Food truck post-ironic lovely layout hashtag seitan.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-item">
+                    <h4>increase profit</h4>
+                    <div class="dev"></div>
+                    <i class="fimanager flaticon-029-financial"></i>
+                    <div class="hover-effect">
+                        <p>Lorem ipsum dolor amet you  probably haven't heard of them aster literally jean shorts woke. Food truck post-ironic lovely layout hashtag seitan.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="service-item">
+                    <h4>certified business</h4>
+                    <div class="dev"></div>
+                    <i class="fimanager flaticon-003-announcement"></i>
+                    <div class="hover-effect">
+                        <p>Lorem ipsum dolor amet you  probably haven't heard of them aster literally jean shorts woke. Food truck post-ironic lovely layout hashtag seitan.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
