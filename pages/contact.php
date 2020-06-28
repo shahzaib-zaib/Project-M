be2f1196d58a84c5aaf731409bb78e0728f34382
Shahzaib <shahzaibux@gmail.com>
2020-06-29 04:28:32 +0500

    Updated Project

--- pages/contact.php
+++ pages/contact.php
@@ -10,3 +10,45 @@
         </div>
     </div>
 </div>
+<div class="contact-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="left-info">
+                    <ul>
+                        <li>
+                            <i class="fa fa-phone"></i>
+                            <h6>Phone Number</h6>
+                            <span>+123 4567 8910</span>
+                        </li>
+                        <li>
+                            <i class="fa fa-envelope"></i>
+                            <h6>Email Address</h6>
+                            <span>contact@rosaleda.com</span>
+                        </li>
+                        <li>
+                            <i class="fa fa-globe"></i>
+                            <h6>Website</h6>
+                            <span>www.rosaleda.com</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <form id="ajax-contact" method="post" action="#">
+                    <div class="input-field">
+                        <input type="text" class="form-control" name="name" id="name" required placeholder="Name">
+                    </div>
+                    <div class="input-field">
+                        <input type="email" class="form-control" name="email" id="email" required placeholder="Email">
+                    </div>
+                    <div class="input-field">
+                        <textarea class="form-control" name="message" id="message" required placeholder="Message"></textarea>
+                    </div>
+                    <button class="button main-button" type="submit">send message</button>
+                </form>
+                <div id="form-messages" class="mt-3"></div>
+            </div>
+        </div>
+    </div>
+</div>
