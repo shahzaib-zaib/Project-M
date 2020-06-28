f99ef3ae54fd26d3c3d0a88479e1cfa162ac284e
Shahzaib <shahzaibux@gmail.com>
2020-06-29 04:37:52 +0500

    Updated Project

---
+++ pages/faqs.php
@@ -0,0 +1,12 @@
+<div class="page-heading">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-heading-content">
+                    <span><a href="index.html">Home</a>/<em>FAQ's</em></span>
+                    <h1>FAQ's</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
