1c85611ee99c307b796c4436c61965d4eb751156
Shahzaib <shahzaibux@gmail.com>
2020-06-29 03:47:26 +0500

    Updated Project

---
+++ part/footer.php
@@ -0,0 +1,105 @@
+<footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="footer-item">
+                            <div class="footer-logo">
+                                <img src="images/logo-white.png" alt="">
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit. Nulla animi, rep ellat, repellend possimus iure.<br><br>Tenetur enterance and correct bland itiis culpa unde explicabo.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="footer-item useful-links">
+                            <h4>Useful Links</h4>
+                            <ul>
+                                <li><a href="#">Homepage</a></li>
+                                <li><a href="#">About</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Clients</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="footer-item blog-entries">
+                            <h4>Recent Posts</h4>
+                            <ul>
+                                <li>
+                                    <a href="single-post.html">
+                                        <img src="http://placehold.it/80x80" alt="">
+                                        <p>Lorem ipsum dolor amet fast vlora istikam ...</p>
+                                        <span><i class="fa fa-calendar"></i>Nov 09, 2019</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="single-post.html">
+                                        <img src="http://placehold.it/80x80" alt="">
+                                        <p>Lorem ipsum dolor amet fast vlora istikam ...</p>
+                                        <span><i class="fa fa-calendar"></i>Nov 02, 2019</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-3">
+                        <div class="footer-item latest-projects">
+                            <h4>Lastest Projects</h4>
+                            <ul>
+                                <li><a href="single-project.html"><img src="http://placehold.it/80x80" alt=""></a></li>
+                                <li><a href="single-project.html"><img src="http://placehold.it/80x80" alt=""></a></li>
+                                <li><a href="single-project.html"><img src="http://placehold.it/80x80" alt=""></a></li>
+                                <li><a href="single-project.html"><img src="http://placehold.it/80x80" alt=""></a></li>
+                                <li><a href="single-project.html"><img src="http://placehold.it/80x80" alt=""></a></li>
+                                <li><a href="single-project.html"><img src="http://placehold.it/80x80" alt=""></a></li>
+                                <li><a href="single-project.html"><img src="http://placehold.it/80x80" alt=""></a></li>
+                                <li><a href="single-project.html"><img src="http://placehold.it/80x80" alt=""></a></li>
+                                <li><a href="single-project.html"><img src="http://placehold.it/80x80" alt=""></a></li>
+                                <li><a href="single-project.html"><img src="http://placehold.it/80x80" alt=""></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-12">
+                        <div class="under-footer">
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <i class="fa fa-phone"></i>
+                                    <h6>Phone</h6>
+                                    <span>0123 456 789</span> 
+                                </div>
+                                <div class="col-md-4">
+                                    <i class="fa fa-envelope"></i>
+                                    <h6>Mail</h6>
+                                    <span>contact@rosaleda.com</span>
+                                </div>
+                                <div class="col-md-4">
+                                    <i class="fa fa-map-marker"></i>
+                                    <h6>Location</h6>
+                                    <span>Street 50, South Bend, North Str</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+        <div class="sub-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="copyright-text">
+                            <p> All Rights Reserved. <a href="http://buttoncreative.agency">ButtonCreative</a> 2020.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <ul>
+                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
