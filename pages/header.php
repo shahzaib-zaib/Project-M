3c298b3ad5f78f2b1a13fb5183c102ef2110bf37
Shahzaib <shahzaibux@gmail.com>
2020-06-29 01:26:17 +0500

    Updated Project

---
+++ pages/header.php
@@ -0,0 +1,44 @@
+<div class="preloader">
+            <div class="preloader-bounce">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </div>
+        
+        <header class="header-area header-sticky">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <nav class="main-nav">
+                            <!-- ***** Logo Start ***** -->
+                            <a href="home.html" class="logo">
+                                <img src="images/logo.png" alt="">
+                            </a>
+                            <!-- ***** Logo End ***** -->
+                            <!-- ***** Menu Start ***** -->
+                            <ul class="nav">
+                                <li><a href="home.html" class="active">Home</a></li>
+                                <li><a href="about-us.html">About</a></li>
+                                <li class="submenu">
+                                    <a href="javascript:;">Services</a>
+                                    <ul>
+                                        <li><a href="services.html">Child Services</a></li>
+                                        <li><a href="team.html">Family Services</a></li>
+                                        <li><a href="single-project.html">Adult Services</a></li>
+                                        <li><a href="single-post.html">Corporate Services</a></li>
+                                        <li><a href="single-member.html">Medico Legal</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="blog.html">Consultation</a></li>
+                                <li><a href="contact-us.html">Contact</a></li> 
+                            </ul>        
+                            <a class='menu-trigger'>
+                                <span>Menu</span>
+                            </a>
+                            <!-- ***** Menu End ***** -->
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
