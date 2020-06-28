6e4d779250316a05f170e13db60afa0ea921b5ba
Shahzaib <shahzaibux@gmail.com>
2020-06-29 03:52:33 +0500

    Updated Project

--- pages/about.php
+++ pages/about.php
@@ -10,3 +10,36 @@
         </div>
     </div>
 </div>
+<div class="why-choose-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <div class="section-heading">
+                    <span>Why Choose Us</span>
+                    <h2>some reasons why to choose us</h2>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="feature-item">
+                    <h4>OUR MISSION</h4>
+                    <p>Hello lo-fi bespoke banh mi snackwave green juice franzen offal tote bag check chillwave distillery mumblecore.</p>
+                    <span>1</span>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="feature-item">
+                    <h4>OUR ADVANTAGES</h4>
+                    <p>Hello lo-fi bespoke banh mi snackwave green juice franzen offal tote bag check chillwave distillery mumblecore.</p>
+                    <span>2</span>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="feature-item">
+                    <h4>OUR GUARANTEES</h4>
+                    <p>Hello lo-fi bespoke banh mi snackwave green juice franzen offal tote bag check chillwave distillery mumblecore.</p>
+                    <span>3</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
