a1dc191e1258943e2c642f277e006ed768ff9d01
Shahzaib <shahzaibux@gmail.com>
2020-06-29 04:41:57 +0500

    Updated Project

---
+++ pages/services.php
@@ -0,0 +1,12 @@
+<div class="page-heading">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-heading-content">
+                    <span><a href="index.html">Home</a>/<em>Our Services</em></span>
+                    <h1>Our Services</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
