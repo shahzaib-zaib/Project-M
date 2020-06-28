52e7f7aa86c3b2f8fde677e618be53f05b4b8bc5
Shahzaib <shahzaibux@gmail.com>
2020-06-29 04:17:02 +0500

    Updated Project

---
+++ pages/contact.php
@@ -0,0 +1,12 @@
+<div class="page-heading">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-heading-content">
+                    <span><a href="index.html">Home</a>/<em>Contact Us</em></span>
+                    <h1>Contact Us</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
