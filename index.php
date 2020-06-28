61eaee5c9dbf9f4a34610e9c85fbc4e38a5382ee
Shahzaib <shahzaibux@gmail.com>
2020-06-29 01:06:42 +0500

    Updated Project

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Dr. Maheen Rizvi</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title>Rosaleda - Creative HTML5 Template</title>
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+
+    <link rel="stylesheet" type="text/css" href="css/fullpage.min.css">
+
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+
+    <link rel="stylesheet" href="css/animate.css">
+
+    <link rel="stylesheet" href="css/flaticon.css">
+
+    <link rel="stylesheet" href="css/rosaleda-multipage.css">
+
+    <link rel="stylesheet" href="css/rosaleda-onepage.css">
+</head>
+<body>
+    
+</body>
+</html>
