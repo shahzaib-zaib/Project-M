3a910bc75feba9e165c745327fba415166dc2841
Shahzaib <shahzaibux@gmail.com>
2020-06-29 04:39:07 +0500

    Updated Project

--- pages/faqs.php
+++ pages/faqs.php
@@ -10,3 +10,67 @@
         </div>
     </div>
 </div>
+<div class="faqs-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <ul class="accordion">
+                    <li>
+                        <a>What a product key?</a>
+                        <div class="content">
+                            <p>90'As schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke.<br><br>
+                            <img src="http://placehold.it/370x300" alt="">Vexillologist listicle glossier ugh post-ironic. Ethical scenester irony organic disrupt. Lumbersexual four loko wolf art party helvetica cold-pressed polaroid biodiesel tacos quinoa tilde keffiyeh woke. Edison bulb meggings shaman bushwick iPhone lo-fi.
+                            A schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke. Tofu vexillologist raclette bushwick copper mug keytar. Bespoke vape 3 wolf moon.<br><br>Hella forage letterpress selvage hexagon. Fanny pack portland brooklyn artisan yuccie whatever. Scenester hammock cred pabst, chambray tbh distillery small batch synth brunch cold-pressed actually. swag messenger bag, actually fanny pack photo booth mustache mixtape bespoke taiyaki raw denim knausgaard four loko. Tacos synth tumeric pickled, iPhone bushwick cred helvetica ethical cray sustainable. Leggings church-key shabby chic subway tile gastropub.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a>Where do i find my product key?</a>
+                        <div class="content">
+                            <p>
+                            Vexillologist listicle glossier ugh post-ironic. Ethical scenester irony organic disrupt. Lumbersexual four loko wolf art party helvetica cold-pressed polaroid biodiesel tacos quinoa tilde keffiyeh woke. Edison bulb meggings shaman bushwick iPhone lo-fi.
+                            A schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke. Tofu vexillologist raclette bushwick copper mug keytar. Bespoke vape 3 wolf moon narwhal +1 authentic coloring book chartreuse.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a>How long does it take to download?</a>
+                        <div class="content">
+                            <p>
+                            <img src="http://placehold.it/370x300" alt="">Vexillologist listicle glossier ugh post-ironic. Ethical scenester irony organic disrupt. Lumbersexual four loko wolf art party helvetica cold-pressed polaroid biodiesel tacos quinoa tilde keffiyeh woke. Edison bulb meggings shaman bushwick iPhone lo-fi.
+                            A schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke. Tofu vexillologist raclette bushwick copper mug keytar. Bespoke vape 3 wolf moon.<br><br>Hella forage letterpress selvage hexagon. Fanny pack portland brooklyn artisan yuccie whatever. Scenester hammock cred pabst, chambray tbh distillery small batch synth brunch cold-pressed actually. swag messenger bag, actually fanny pack photo booth mustache mixtape bespoke taiyaki raw denim knausgaard four loko. Tacos synth tumeric pickled, iPhone bushwick cred helvetica ethical cray sustainable. Leggings church-key shabby chic subway tile gastropub.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a>What steps to follow after download?</a>
+                        <div class="content">
+                            <p>90'As schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke.<br><br>
+                            Vexillologist listicle glossier ugh post-ironic. Ethical scenester irony organic disrupt. Lumbersexual four loko wolf art party helvetica cold-pressed polaroid biodiesel tacos quinoa tilde keffiyeh woke. Edison bulb meggings shaman bushwick iPhone lo-fi.
+                            A schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke. Tofu vexillologist raclette bushwick copper mug keytar. Bespoke vape 3 wolf moon narwhal +1 authentic coloring book chartreuse</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a>More information about install</a>
+                        <div class="content">
+                            <p>90'As schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke.<br><br>
+                            <img src="http://placehold.it/370x300" alt="">Vexillologist listicle glossier ugh post-ironic. Ethical scenester irony organic disrupt. Lumbersexual four loko wolf art party helvetica cold-pressed polaroid biodiesel tacos quinoa tilde keffiyeh woke. Edison bulb meggings shaman bushwick iPhone lo-fi.
+                            A schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke. Tofu vexillologist raclette bushwick copper mug keytar. Bespoke vape 3 wolf moon.<br><br>Hella forage letterpress selvage hexagon. Fanny pack portland brooklyn artisan yuccie whatever. Scenester hammock cred pabst, chambray tbh distillery small batch synth brunch cold-pressed actually. swag messenger bag, actually fanny pack photo booth mustache mixtape bespoke taiyaki raw denim knausgaard four loko. Tacos synth tumeric pickled, iPhone bushwick cred helvetica ethical cray sustainable. Leggings church-key shabby chic subway tile gastropub.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a>What comes next?</a>
+                        <div class="content">
+                            <p>Hella forage letterpress selvage hexagon. Fanny pack portland brooklyn artisan yuccie whatever. Scenester hammock cred pabst, chambray tbh distillery small batch synth brunch cold-pressed actually. swag messenger bag, actually fanny pack photo booth mustache mixtape bespoke taiyaki raw denim knausgaard four loko. Tacos synth tumeric pickled, iPhone bushwick cred helvetica ethical cray sustainable. Leggings church-key shabby chic subway tile gastropub.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a>How do i get the best resolution?</a>
+                        <div class="content">
+                            <p>90'As schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke.<br><br>
+                            Vexillologist listicle glossier ugh post-ironic. Ethical scenester irony organic disrupt. Lumbersexual four loko wolf art party helvetica cold-pressed polaroid biodiesel tacos quinoa tilde keffiyeh woke. Edison bulb meggings shaman bushwick iPhone lo-fi.
+                            A schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke. Tofu vexillologist raclette bushwick copper mug keytar. Bespoke vape 3 wolf moon narwhal +1 authentic coloring book chartreuse.</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
