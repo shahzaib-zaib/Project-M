5d3754e5a5758cbedd3ec6a196b693804db23626
Shahzaib <shahzaibux@gmail.com>
2020-07-01 17:06:34 +0500

    Updated Project

---
+++ header.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Dr. Maheen Rizvi</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/fullpage.min.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/rosaleda-multipage.css">
+    <link rel="stylesheet" type="text/css" href="../assets/css/rosaleda-onepage.css">
+</head>
+<body>
+    <div class="preloader">
+        <div class="preloader-bounce">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="home.html" class="logo">
+                            <img src="images/logo.png" alt="">
+                        </a>
+                        <!-- ***** Logo End ***** -->
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li><a href="home.html" class="active">Home</a></li>
+                            <li><a href="about-us.html">About</a></li>
+                            <li class="submenu">
+                                <a href="javascript:;">Services</a>
+                                <ul>
+                                    <li><a href="services.html">Child Services</a></li>
+                                    <li><a href="team.html">Family Services</a></li>
+                                    <li><a href="single-project.html">Adult Services</a></li>
+                                    <li><a href="single-post.html">Corporate Services</a></li>
+                                    <li><a href="single-member.html">Medico Legal</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="blog.html">Consultation</a></li>
+                            <li><a href="contact-us.html">Contact</a></li> 
+                        </ul>        
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    
+
+    <script src="../assets/js/jquery.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/fullpage.min.js"></script>
+    <script src="../assets/js/scrolloverflow.js"></script>
+    <script src="../assets/js/owl.carousel.min.js"></script>
+    <script src="../assets/js/accordions.js"></script>
+    <script src="../assets/js/jquery.inview.min.js"></script>
+    <script src="../assets/js/form.js"></script>
+    <script src="../assets/js/isotope.js"></script>
+    <script src="../assets/js/waypoints.min.js"></script>
+    <script src="../assets/js/custom.js"></script>
+</body>../
+</html>
