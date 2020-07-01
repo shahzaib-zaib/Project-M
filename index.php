646c9b4a3c7f9bccbd4c7f32f25f8b8334a5fec7
Shahzaib <shahzaibux@gmail.com>
2020-07-01 16:00:04 +0500

    Updated Project

--- index.php
+++ index.php
@@ -9,54 +9,255 @@
 
     <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 
-    <title>Rosaleda - Creative HTML5 Template</title>
-
     <!-- Additional CSS Files -->
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-
     <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
-
     <link rel="stylesheet" type="text/css" href="css/fullpage.min.css">
-
     <link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
-
     <link rel="stylesheet" href="css/animate.css">
-
     <link rel="stylesheet" href="css/flaticon.css">
-
     <link rel="stylesheet" href="css/rosaleda-multipage.css">
-
     <link rel="stylesheet" href="css/rosaleda-onepage.css">
 </head>
 <body>
+    <div class="main-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="decoration">
+                        <img src="images/banner-decoration.png" alt="">
+                    </div>
+                    <div class="welcome-box">
+                        <h1>We are <span class="txt-type" data-wait="3000" data-words='["La Rosaleda", "Creative", "Unique"]'></span></h1>
+                        <p>minimal <em>template</em></p>
+                        <div class="scroll-down next-section">
+                            <img src="images/mouse-scroll.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="section animated-row slide-02" data-section="slide02" style="margin: 100px 0 100px 0;">
+        <div class="section-inner">
+            <div class="about-section">
+                <div class="row justify-content-center">
+                    <div class="col-lg-10 wide-col-laptop">
+                        <div class="row">
+                            <div class="col-md-5 align-self-center mt-5">
+                                <div class="about-contentbox">
+                                    <div>
+                                        <div class="section-heading">
+                                            <span>WELCOME TO OUR WEBSITE</span>
+                                            <h2>Our lorem ipsum <em>is to dolor</em> ametis postekam dohaer sistikaf amet</h2>
+                                            <img src="images/square-decoration.png" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-7">
+                                <div id='tabs'>
+                                    <ul>
+                                    <li><a href='#tabs-1'>OUR MISSION
+                                    </a></li>
+                                    <li><a href='#tabs-2'>OUR ADVANTAGES</a></li>
+                                    <li><a href='#tabs-3'>OUR GUARANTEES</a></li>
+                                    </ul>
+                                    <section class='tabs-content'>
+                                    <article id='tabs-1'>
+                                        <p>Farm-to-table af direct trade tumblr, bespoke paleo venmo PBRBsta neutra vegan craft beer echo park. Echo park hell of ugh, synthicasti coloring book cardigan truffaut hexagon dreamcatcher microdosingil este.<br><br>Farm-to-table af direct trade tumblr, bespoke paleo venmo PBRBsta lorem ipsum dolor amet ningun postedam postekaris vegan craft beer echo park. Echo park hell of ugh, synthicasti coloring book cardigan.</p>
+                                        <a href="#">More about us</a>
+                                    </article>
+                                    <article id='tabs-2'>
+                                        <p>Farm-to-table af direct trade tumblr, bespoke paleo venmo PBRBsta neutra vegan craft beer echo park. Echo park hell of ugh, synthicasti coloring book cardigan truffaut hexagon dreamcatcher microdosingil este.<br><br>Farm-to-table af direct trade tumblr, bespoke paleo venmo PBRBsta lorem ipsum dolor amet ningun postedam postekaris istifaris ostekam et valar dohaeris istikam poredam ostekam poredam valar dohaeris is tifar as.</p>
+                                        <a href="#">More about us</a>
+                                    </article>
+                                    <article id='tabs-3'>
+                                        <p>Farm-to-table af direct trade tumblr, bespoke paleo venmo PBRBsta neutra vegan craft beer echo park. Echo park hell of ugh, synthicasti coloring book cardigan truffaut hexagon dreamcatcher microdosingil este.<br><br>Farm-to-table af direct trade tumblr, bespoke paleo venmo PBRBsta lorem ipsum dolor amet ningun postedam postekaris istifaris ostekam et valar dohaeris istikam vegan craft beer echo park. Echo park hell of ugh, synthicasti coloring book cardigan.</p>
+                                        <a href="#">More about us</a>
+                                    </article>
+                                    </section>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     
+    <div class="get-to-know-what-we-do">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="home-section-heading">
+                        <span>Get to know what we do</span>
+                        <h2>Get familiar with <em>our services</em></h2>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="service-item">
+                        <h4>ready to collaboration</h4>
+                        <div class="dev"></div>
+                        <i class="fimanager flaticon-017-collaboration"></i>
+                        <div class="hover-effect">
+                            <p>Lorem ipsum dolor amet you  probably haven't heard of them aster literally jean shorts woke. Food truck post-ironic lovely layout hashtag seitan.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="service-item">
+                        <h4>Fixes &amp; improvements</h4>
+                        <div class="dev"></div>
+                        <i class="fimanager flaticon-018-corporate"></i>
+                        <div class="hover-effect">
+                            <p>Lorem ipsum dolor amet you  probably haven't heard of them aster literally jean shorts woke. Food truck post-ironic lovely layout hashtag seitan.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="service-item">
+                        <h4>healthy pertnership</h4>
+                        <div class="dev"></div>
+                        <i class="fimanager flaticon-044-partnership"></i>
+                        <div class="hover-effect">
+                            <p>Lorem ipsum dolor amet you  probably haven't heard of them aster literally jean shorts woke. Food truck post-ironic lovely layout hashtag seitan.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="purchase-rosaleda">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2">
+                    <div class="home-section-heading">
+                        <span>La rosaleda HTML5 Templa</span>
+                        <h2>Rosaleda is a multipurpose template with <em>unique identity</em></h2>
+                    </div>
+                </div>
+                <div class="col-md-8 align-self-center">
+                    <div class="left-image">
+                        <img src="http://placehold.it/670x280" alt="">
+                    </div>
+                </div>
+                <div class="col-md-4 align-self-center">
+                    <ul>
+                        <li><span>01</span>Unique Identity</li>
+                        <li><span>02</span>Creative Layout</li>
+                        <li><span>03</span>Clean &amp; Minimal</li>
+                        <li><span>04</span>Professional Work</li>
+                    </ul>
+                </div>
+                <div class="col-md-12">
+                    <div class="center-button">
+                        <a href="#" class="button main-button">Buy  Now!</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
+    <div class="fun-facts home-facts">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="fact-item">
+                        <div class="counter-box">
+                            <span class="count-number">14</span> <em>Winning Awards</em>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="fact-item">
+                        <div class="counter-box">
+                            <span class="count-number">210</span> <em>Projects Done</em>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="fact-item">
+                        <div class="counter-box">
+                            <span class="count-number">950</span> <em>Working Hours</em>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="fact-item">
+                        <div class="counter-box">
+                            <span class="count-number">84</span> <em>Happy Clients</em>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-
-
-
+    <div class="frequently-asked">
+        <div class="container">
+            <div class="row">
+                <div class="decoration-square">
+                    <img src="images/square-decoration.png" alt="">
+                </div>
+                <div class="col-md-6 align-self-center">
+                    <div class="home-section-heading">
+                        <span>faq's</span>
+                        <h2>Most <em>frequently asked questions</em> by some of the clients</h2>
+                        <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type if specimen book. Lorem Ipsum has been the industry's standard dummy text ever a since the 1500s, when an unknown printer.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 align-self-center">
+                    <ul class="accordion">
+                        <li>
+                            <a>What a product key?</a>
+                            <div class="content">
+                                <p>Lorem ipsum dolor amet succulents put a bird on it pabst fanny pack, kinfolk literally authentic microdosing cardigan cronut letterpress coloring book direct trade leggings.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a>Where do i find my product key?</a>
+                            <div class="content">
+                                <p>Lorem ipsum dolor amet succulents put a bird on it pabst fanny pack, kinfolk literally authentic microdosing cardigan cronut letterpress coloring book direct trade leggings.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a>How long does it take to download?</a>
+                            <div class="content">
+                                <p>Lorem ipsum dolor amet succulents put a bird on it pabst fanny pack, kinfolk literally authentic microdosing cardigan cronut letterpress coloring book direct trade leggings.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a>What steps to follow after download?</a>
+                            <div class="content">
+                                <p>Lorem ipsum dolor amet succulents put a bird on it pabst fanny pack, kinfolk literally authentic microdosing cardigan cronut letterpress coloring book direct trade leggings.</p>
+                            </div>
+                        </li>
+                        <li>
+                            <a>How do i get the best resolution?</a>
+                            <div class="content">
+                                <p>Lorem ipsum dolor amet succulents put a bird on it pabst fanny pack, kinfolk literally authentic microdosing cardigan cronut letterpress coloring book direct trade leggings.</p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="js/jquery.js"></script>
-
     <script src="js/bootstrap.min.js"></script>
-
     <script src="js/fullpage.min.js"></script>
-
     <script src="js/scrolloverflow.js"></script>
-
     <script src="js/owl.carousel.min.js"></script>
-
     <script src="js/accordions.js"></script>
-
     <script src="js/jquery.inview.min.js"></script>
-
     <script src="js/form.js"></script>
-
     <script src="js/isotope.js"></script>
-
     <script src="js/waypoints.min.js"></script>
-
     <script src="js/custom.js"></script>
 </body>
 </html>
