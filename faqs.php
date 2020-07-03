84ef14cffc022cb5edc09889ff274d4c312f0f29
Shahzaib <shahzaibux@gmail.com>
2020-07-04 01:43:02 +0500

    Updated Project

--- faqs.php
+++ faqs.php
@@ -18,11 +18,9 @@
             <h4>What to expect</h4>
                 <ul class="accordion">   
                     <li>
-                        <a>What a product key?</a>
+                        <a>What if I have a complaint about treatment?</a>
                         <div class="content">
-                            <p>90'As schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke.<br><br>
-                            <img src="http://placehold.it/370x300" alt="">Vexillologist listicle glossier ugh post-ironic. Ethical scenester irony organic disrupt. Lumbersexual four loko wolf art party helvetica cold-pressed polaroid biodiesel tacos quinoa tilde keffiyeh woke. Edison bulb meggings shaman bushwick iPhone lo-fi.
-                            A schlitz paleo snackwave everyday carry. Small batch sustainable art party vegan flannel jianbing adaptogen umami yuccie. Kickstarter before they sold out godard banh mi pop-up roof party vexillologist semiotics actually fingerstache taiyaki pug art party 3 wolf moon bespoke. Tofu vexillologist raclette bushwick copper mug keytar. Bespoke vape 3 wolf moon.<br><br>Hella forage letterpress selvage hexagon. Fanny pack portland brooklyn artisan yuccie whatever. Scenester hammock cred pabst, chambray tbh distillery small batch synth brunch cold-pressed actually. swag messenger bag, actually fanny pack photo booth mustache mixtape bespoke taiyaki raw denim knausgaard four loko. Tacos synth tumeric pickled, iPhone bushwick cred helvetica ethical cray sustainable. Leggings church-key shabby chic subway tile gastropub.</p>
+                            <p>If you feel unhappy at any point with our service then please do not hesitate to call us directly and we will review the situation with you and address the issue immediately.<br>
                         </div>
                     </li>
                     <li>
