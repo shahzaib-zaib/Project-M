39f8f1d99ccdd5c306ae248625180ddaec095fa1
Shahzaib <shahzaibux@gmail.com>
2020-07-05 04:11:58 +0500

    Updated Project

--- faqs.php
+++ faqs.php
@@ -48,6 +48,40 @@
                         </div>
                     </li>
                 </ul>
+                
+                <h4>General</h4>
+                <ul class="accordion">   
+                    <li>
+                        <a>What if I have a complaint about treatment?</a>
+                        <div class="content">
+                            <p>If you feel unhappy at any point with our service then please do not hesitate to call us directly and we will review the situation with you and address the issue immediately.<br>
+                        </div>
+                    </li>
+                    <li>
+                        <a>How do I know if the treatment will be beneficial?</a>
+                        <div class="content">
+                            <p>There are no guarantees with psychiatry. I work in this field because I have seen people achieve tremendous gains form this kind of work. That doesn't mean that I can guarantee that every person will be helped, but if what you need is something that I can help with then I will use the best available treatments to do my utmost to help.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a>What are your opening hours?</a>
+                        <div class="content">
+                            <p>My phone is answered between the hours of 9 am to 6 pm Monday to Friday and 9:30 am to 3:30 pm on Saturdays, but you can leave a message outside of these hours and someone will call you back when I am open.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a>Can I refer someone else into treatment?</a>
+                        <div class="content">
+                            <p>Most of my residential treatment enquiries for addictions and alcoholism start with a call from a family member, friend or employer. I have dealt with every kind of difficult situation and I am happy to help you do what can be done. Sometimes if the person is unwilling to address their problem then you may not be able to do anything about them, but I can help and advise you about how to look after yourself and any other people who are being affected, and also how to start to bring pressure to bear on the person so that hopefully they will be willing to get some help.</p>
+                        </div>
+                    </li>
+                    <li>
+                        <a>What issues do you treat?</a>
+                        <div class="content">
+                            <p>It's a very wide range, but the main theme is that I am here to help with anything that is troubling you in your mind; whether it's a slight worry that just needs a little help, through to more serious issues. One thing I won't do is tell you I can help if I can't, so feel free to call me and I can talk it through with you.</p>
+                        </div>
+                    </li>
+                </ul>
             </div>
         </div>
         
