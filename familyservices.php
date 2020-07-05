08a6d845c89dd22a366447203f333f5c0dcd1a49
Shahzaib <shahzaibux@gmail.com>
2020-07-06 03:04:29 +0500

    Updated Project

---
+++ familyservices.php
@@ -0,0 +1,28 @@
+<?php include 'header.php'; ?>
+<div class="page-heading">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-heading-content">
+                    <span><a href="index.html">Home</a>/<em>Family Services</em></span>
+                    <h1>Family Services</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="why-choose-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-heading">
+                    <h3>Private mental health you can trust</h3>
+                    <blockquote><i class="fa fa-quote-left"></i>Dr Maheen and Associates have wide experience of child-care, personal injury, private and public law cases and assessments. Separate details are available on request<i class="fa fa-quote-right"></i></blockquote>
+                    <p>Unfortunately I am not able to accept instructions funded by legal aid.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
