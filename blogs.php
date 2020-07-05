be42e7879166704b4884c57748f4102c7ac69008
Shahzaib <shahzaibux@gmail.com>
2020-07-05 16:40:12 +0500

    Updated Project

--- blogs.php
+++ blogs.php
@@ -1,4 +1,69 @@
 <?php include 'header.php'; ?>
-
+<div class="page-heading">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="page-heading-content">
+                    <span><a href="index.html">Home</a>/<em>Blogs</em></span>
+                    <h1>Blogs</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="blog-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="blog-posts">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="blog-post">
+                                <div class="blog-thumb">
+                                    <img src="http://placehold.it/370x250" alt="">
+                                    <span>Feature, Branding</span>
+                                </div>
+                                <h4>Promotes former official</h4>
+                                <ul>
+                                    <li><a href="#">Admin</a></li>
+                                    <li><a href="#">Nov 09, 2019</a></li>
+                                </ul>
+                                <p>Lorem ipsum dolor amet blog poutine thundercats, kombucha XOXO ethical hoodie lomo blue bottle unicorn. Retro iPhone before they sold out semiotics.</p>
+                                <a href="single-post.html" class="read-more">Read More</a>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="blog-post">
+                                <div class="blog-thumb">
+                                    <img src="http://placehold.it/370x250" alt="">
+                                    <span>Feature, Branding</span>
+                                </div>
+                                <h4>How To write Old Posts</h4>
+                                <ul>
+                                    <li><a href="#">Admin</a></li>
+                                    <li><a href="#">Nov 07, 2019</a></li>
+                                </ul>
+                                <p>Lorem ipsum dolor amet blog poutine thundercats, kombucha XOXO ethical hoodie lomo blue bottle unicorn. Retro iPhone before they sold out semiotics.</p>
+                                <a href="single-post.html" class="read-more">Read More</a>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <ul class="blog-pagination">
+                                <li><a href="#"><i class="fa fa-angle-double-left"></i></a></li>
+                                <li><a href="#">1</a></li>
+                                <li class="active"><a href="#">2</a></li>
+                                <li><a href="#">3</a></li>
+                                <li><a href="#">4</a></li>
+                                <li><a href="#">...</a></li>
+                                <li><a href="#">40</a></li>
+                                <li><a href="#"><i class="fa fa-angle-double-right"></i></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php include 'footer.php'; ?>
