bdfae2496f24b8cc6219d434ffb326aa27de1c65
Shahzaib <shahzaibux@gmail.com>
2020-07-05 21:50:39 +0500

    Updated Project

--- childservices.php
+++ childservices.php
@@ -14,7 +14,7 @@
 <div class="why-choose-us">
     <div class="container">
         <div class="row">
-            <div class="col-md-8 offset-md-2">
+            <div class="col-md-12">
                 <div class="section-heading">
                     <h3>Private mental health for Children and Adolescents</h3>
                     <blockquote><i class="fa fa-quote-left"></i>We are here to help you find the right care for your child or teenager – it’s a big decision and not easy to know who is the best person for your child to see.<br><br>Your child will be seen by one of our senior, experienced clinicians, personally selected for their compassionate nature and commitment to helping families just like yours, either face-to face or online.<i class="fa fa-quote-right"></i></blockquote>
@@ -22,24 +22,46 @@
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="feature-item">
+                <div class="feature-item2">
                     <h4>Child Psychiatry</h4>
                     <p>If you are looking for a diagnosis and a thorough review of the issues your child is experiencing, a child psychiatrist will be the best option for your family</p>
                     <span>1</span>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="feature-item">
+                <div class="feature-item2">
                     <h4>Child Psychology</h4>
                     <p>Offering evidence based treatments, such as CBT, psychologists work with children and families to help overcome challenges and learn skills that last a lifetime</p>
                     <span>2</span>
                 </div>
             </div>
             <div class="col-md-4">
-                <div class="feature-item">
-                    <h4>GUARANTEES</h4>
-                    <p></p>
+                <div class="feature-item2">
+                    <h4>ADHD</h4>
+                    <p>Finding the right treatment option for a child with ADHD can be life changing – suddenly school, social and home life becomes much easier and a child is able to reach their full potential.</p>
                     <span>3</span>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-5">
+            <div class="col-md-4">
+                    <div class="feature-item2">
+                        <h4>Depression</h4>
+                        <p>It is truly heartbreaking to watch a child or teenager suffer with low mood, tearfulness and a loss of interest in all their previous hobbies. With the right diagnosis and support, things can and do get better.</p>
+                        <span>4</span>
+                    </div>
+                </div><div class="col-md-4">
+                    <div class="feature-item2">
+                        <h4>Anxiety</h4>
+                        <p>A child who is experiencing anxiety will often be hugely distressed and find the world to be a very scary place. With the right treatment, these feelings can be significantly improved.</p>
+                        <span>5</span>
+                    </div>
+                </div><div class="col-md-4">
+                    <div class="feature-item2">
+                        <h4>Autism</h4>
+                        <p>Families often speak of the tremendous relief they feel when a child receives a diagnosis of autism – finally they have a way of understanding why their child seems different to others.</p>
+                        <span>6</span>
+                    </div>
                 </div>
             </div>
         </div>
